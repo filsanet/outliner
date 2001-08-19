53779eadb7a1581eff2af56a4135170b1da3fcc1
maynardd <>
2001-08-19 06:16:01 +0000

    Now using helma.xmlrpc.jar rather than xmlrpc.jar

--- outliner/run.bat
+++ outliner/run.bat
@@ -6,8 +6,9 @@ set CP=%root_path%com.organic.maynard.jar
 set CP=%CP%;%root_path%outliner.jar
 set CP=%CP%;%root_path%sax.jar
 set CP=%CP%;%root_path%xp.jar
-set CP=%CP%;%root_path%xmlrpc.jar
+set CP=%CP%;%root_path%helma.xmlrpc.jar
+set CP=%CP%;%root_path%bsh-1_2b1.jar
 
 @echo on
 REM java -cp %CP% -Duser.language=ja -Duser.region=JP -Xincgc com.organic.maynard.outliner.Outliner ja
-java -cp %CP% -Xincgc com.organic.maynard.outliner.Outliner en
+c:\jdk1.3\bin\java -cp %CP% -Xincgc com.organic.maynard.outliner.Outliner en
--- outliner/sendto.bat
+++ outliner/sendto.bat
@@ -11,7 +11,8 @@ set CP=%root_path%com.organic.maynard.jar
 set CP=%CP%;%root_path%outliner.jar
 set CP=%CP%;%root_path%sax.jar
 set CP=%CP%;%root_path%xp.jar
-set CP=%CP%;%root_path%xmlrpc.jar
+set CP=%CP%;%root_path%helma.xmlrpc.jar
+set CP=%CP%;%root_path%bsh-1_2b1.jar
 
 @echo on
 REM java -cp %CP% -Duser.language=ja -Duser.region=JP -Xincgc com.organic.maynard.outliner.Outliner ja %1
