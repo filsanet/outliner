dbd03964a7b35cf94e3b0957e2b53a52ea3cdd83
maynardd <>
2001-10-09 01:26:53 +0000

    shell script for unix to run JOE. Contributed by tswoam.

---
+++ outliner/run_joe.sh
@@ -0,0 +1,31 @@
+#!/bin/sh 
+
+ulimit -s 2048 
+
+INSTALL_DIR=/home/simon/.bin/outliner 
+
+CWD=`pwd` 
+
+FILENAME=`echo $1 | sed -e 's/.*\/\([^\/]*\)/\1/'` 
+if [ "$FILENAME" != "" ]; then 
+FILENAME="$CWD/$FILENAME" 
+else 
+FILENAME="" 
+fi 
+
+cd $INSTALL_DIR 
+
+RP=lib 
+
+CP=$RP/com.organic.maynard.jar 
+CP=$CP:$RP/outliner.jar 
+CP=$CP:$RP/sax.jar 
+CP=$CP:$RP/xp.jar 
+CP=$CP:$RP/helma.xmlrpc.jar 
+CP=$CP:$RP/bsh-1_2b1.jar 
+CP=$CP:$RP/jakarta-oro-2.0.4.jar 
+CP=$CP:$RP/jmousewheel.jar 
+
+java -cp $CP -Xincgc com.organic.maynard.outliner.Outliner en $FILENAME 
+
+cd $CWD
