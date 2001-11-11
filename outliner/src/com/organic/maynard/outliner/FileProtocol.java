77be150f352b8200b4806ebe66e54fc5c2c65b5c
maynardd <>
2001-11-11 10:00:51 +0000

    updated javadocs.

--- outliner/src/com/organic/maynard/outliner/FileProtocol.java
+++ outliner/src/com/organic/maynard/outliner/FileProtocol.java
@@ -18,60 +18,85 @@
 
 package com.organic.maynard.outliner;
 
-public interface FileProtocol { 
-	
+public interface FileProtocol {
+	/**
+	 * Used by the <code>selectFileToSave</code> method to indicate that the 
+	 * GUI should be configured for a save.
+	 */	
 	public static final int SAVE = 0;
+	
+	/**
+	 * Used by the <code>selectFileToSave</code> method to indicate that the 
+	 * GUI should be configured for an export.
+	 */	
 	public static final int EXPORT = 1;
 	
 	/**
 	 * Gets the name of this protocol.
-	 * @return The protocol's unique name.
+	 *
+	 * @return this protocol's unique name.
 	 */
 	public String getName();
 
 	/**
-	 * Sets the name of this protocol. This name must be unique. The name is also
-	 * used for protocol selection in the GUI.
-	 * @param name The protocol's unique name.
+	 * Sets the name of this protocol. This name must be unique. The name is 
+	 * also used for protocol selection in the GUI.
+	 *
+	 * @param name sets this protocol's unique name.
 	 */	
 	public void setName(String name);
 	
 	/**
-	 * Handles selection of a file to open. All GUI elements involved in the selection
-	 * process must be managed by this method. The results of this selection process
-	 * are placed in the provided DocumentInfo object. It is strongly suggested that the
-	 * GUI allow the user to select an OpenFileFormat and an encoding type.
-	 * @param docInfo The DocumentInfo object to store the results of the selection
-	 *                process.
-	 * @return True indicates sucess and false indicates failure.
+	 * Handles selection of a file to open. All GUI elements involved in the 
+	 * selection process must be managed by this method. The results of this 
+	 * selection process are placed in the provided <code>DocumentInfo</code> 
+	 * object. It is strongly suggested that the GUI allow the user to select 
+	 * an <code>OpenFileFormat</code> and an <code>encoding type</code>.
+	 *
+	 * @param docInfo the <code>DocumentInfo</code> object to store the results 
+	 *                of the selection process.
+	 * @return        <code>true</code> indicates success and <code>false</code> 
+	 *                indicates failure.
 	 */		
 	public boolean selectFileToOpen(DocumentInfo docInfo);
 
 	/**
-	 * Handles selection of a file to save. All GUI elements involved in the selection
-	 * process must be managed by this method. The results of this selection process
-	 * are placed in the provided OutlinerDocument's associated DocumentInfo object. 
-	 * It is strongly suggested that the GUI allow the user to select an OpenFileFormat, 
-	 * line ending (platform) and an encoding type.
-	 * @param document The OutlinerDocument that contains the DocumentInfo object to
-	 *                 store the results of the selection process.
-	 * @param type Indicates if this is a Save or an Export.
-	 * @return True indicates sucess and false indicates failure.
+	 * Handles selection of a file to save. All GUI elements involved in the 
+	 * selection process must be managed by this method. The results of this 
+	 * selection process are placed in the provided <code>OutlinerDocument's</code> 
+	 * associated <code>DocumentInfo<code> object. It is strongly suggested 
+	 * that the GUI allow the user to select an <code>OpenFileFormat</code>, 
+	 * line ending (platform) and an <code>encoding type</code>.
+	 *
+	 * @param document the <code>OutlinerDocument</code> that contains the 
+	 *                 <code>DocumentInfo</code> object to store the results 
+	 *                 of the selection process.
+	 * @param type     indicates if this is a Save or an Export.
+	 * @return         <code>true</code> indicates success and <code>false</code> 
+	 *                 indicates failure.
 	 */	
 	public boolean selectFileToSave(OutlinerDocument document, int type);
-	
-	
+
 	/**
-	 * Saves the data stored in the DocumentInfo. The byte[] data is pulled
+	 * Saves the data stored in the DocumentInfo. The <code>byte[]</code> data is pulled
 	 * from the DocumentInfo object's getOutputBytes method.
-	 * @return True indicates sucess and false indicates failure.
+	 *
+	 * @param docInfo the <code>DocumentInfo</code> object that holds the 
+	 *                <code>byte[]</code> array to save.
+	 * @return        <code>true</code> indicates success and <code>false</code> 
+	 *                indicates failure.
 	 */		
 	public boolean saveFile(DocumentInfo docInfo);
 
 	/**
-	 * Open a file and stores an InputStream in the DocumentInfo. The InputStream
-	 * is stored by using the DocumentInfo object's setInputStream method.
-	 * @return True indicates sucess and false indicates failure.
+	 * Open a file and stores an <code>InputStream</code> in the <code>DocumentInfo</code>. 
+	 * The <code>InputStream</code> is stored by using the <code>DocumentInfo</code> 
+	 * object's <code>setInputStream</code> method.
+	 *
+	 * @param docInfo the <code>DocumentInfo</code> object that holds the 
+	 *                <code>InputStream</code> for the file to open.
+	 * @return        <code>true</code> indicates success and <code>false</code> 
+	 *                indicates failure.
 	 */	
 	public boolean openFile(DocumentInfo docInfo);
 }
