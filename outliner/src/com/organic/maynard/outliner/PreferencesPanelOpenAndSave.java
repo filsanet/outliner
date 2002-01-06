e8f072fd0c2905ad2ad4548275e21215c6314028
stanley_krute <>
2002-01-06 09:47:14 +0000

    working on import mode

--- outliner/src/com/organic/maynard/outliner/PreferencesPanelOpenAndSave.java
+++ outliner/src/com/organic/maynard/outliner/PreferencesPanelOpenAndSave.java
@@ -1,5 +1,6 @@
 /**
- * Copyright (C) 2000, 2001 Maynard Demmon, maynard@organic.com
+ * Portions copyright (C) 2000, 2001 Maynard Demmon, maynard@organic.com
+ * Portions copyright (C) 2002   Stan Krute <Stan@StanKrute.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or
@@ -22,7 +23,7 @@
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
  * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
  * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
@@ -31,6 +32,12 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+
+/**
+ * @author  $Author$
+ * @version $Revision$, $Date$
+ */
+ 
 
 package com.organic.maynard.outliner;
 
@@ -43,50 +50,54 @@ import org.xml.sax.*;
 
 public class PreferencesPanelOpenAndSave extends AbstractPreferencesPanel implements PreferencesPanel, GUITreeComponent {
 
- public void endSetup(AttributeList atts) {
-  super.endSetup(atts);
+	public void endSetup(AttributeList atts) {
+		super.endSetup(atts);
 
-  AbstractPreferencesPanel.addArrayToComboBox(Preferences.FILE_PROTOCOLS.toArray(), GUITreeComponentRegistry.COMPONENT_FILE_PROTOCOL);
-  AbstractPreferencesPanel.addArrayToComboBox(PlatformCompatibility.PLATFORM_IDENTIFIERS, GUITreeComponentRegistry.COMPONENT_LINE_ENDING);
-  AbstractPreferencesPanel.addArrayToComboBox(Preferences.ENCODINGS.toArray(), GUITreeComponentRegistry.COMPONENT_ENCODING_WHEN_OPENING);
-  AbstractPreferencesPanel.addArrayToComboBox(Preferences.ENCODINGS.toArray(), GUITreeComponentRegistry.COMPONENT_ENCODING_WHEN_SAVING);
-  AbstractPreferencesPanel.addArrayToComboBox(Preferences.FILE_FORMATS_OPEN.toArray(), GUITreeComponentRegistry.COMPONENT_FORMAT_WHEN_OPENING);
-  AbstractPreferencesPanel.addArrayToComboBox(Preferences.FILE_FORMATS_SAVE.toArray(), GUITreeComponentRegistry.COMPONENT_FORMAT_WHEN_SAVING);
- }
+		AbstractPreferencesPanel.addArrayToComboBox(Preferences.FILE_PROTOCOLS.toArray(), GUITreeComponentRegistry.COMPONENT_FILE_PROTOCOL);
+		AbstractPreferencesPanel.addArrayToComboBox(PlatformCompatibility.PLATFORM_IDENTIFIERS, GUITreeComponentRegistry.COMPONENT_LINE_ENDING);
+		AbstractPreferencesPanel.addArrayToComboBox(Preferences.ENCODINGS.toArray(), GUITreeComponentRegistry.COMPONENT_ENCODING_WHEN_OPENING);
+		AbstractPreferencesPanel.addArrayToComboBox(Preferences.ENCODINGS.toArray(), GUITreeComponentRegistry.COMPONENT_ENCODING_WHEN_IMPORTING);
+		AbstractPreferencesPanel.addArrayToComboBox(Preferences.ENCODINGS.toArray(), GUITreeComponentRegistry.COMPONENT_ENCODING_WHEN_SAVING);
+		AbstractPreferencesPanel.addArrayToComboBox(Preferences.ENCODINGS.toArray(), GUITreeComponentRegistry.COMPONENT_ENCODING_WHEN_EXPORTING);
+		AbstractPreferencesPanel.addArrayToComboBox(Preferences.FILE_FORMATS_OPEN.toArray(), GUITreeComponentRegistry.COMPONENT_FORMAT_WHEN_OPENING);
+		AbstractPreferencesPanel.addArrayToComboBox(Preferences.FILE_FORMATS_IMPORT.toArray(), GUITreeComponentRegistry.COMPONENT_FORMAT_WHEN_IMPORTING);
+		AbstractPreferencesPanel.addArrayToComboBox(Preferences.FILE_FORMATS_SAVE.toArray(), GUITreeComponentRegistry.COMPONENT_FORMAT_WHEN_SAVING);
+		AbstractPreferencesPanel.addArrayToComboBox(Preferences.FILE_FORMATS_EXPORT.toArray(), GUITreeComponentRegistry.COMPONENT_FORMAT_WHEN_EXPORTING);
+	}
 
- public void applyCurrentToApplication() {
-  Preferences prefs = (Preferences) GUITreeLoader.reg.get(GUITreeComponentRegistry.PREFERENCES);
-  PreferenceString pFileProtocol = (PreferenceString) prefs.getPreference(Preferences.FILE_PROTOCOL);
-  PreferenceLineEnding pLineEnd = (PreferenceLineEnding) prefs.getPreference(Preferences.SAVE_LINE_END);
-  PreferenceString pSaveEncoding = (PreferenceString) prefs.getPreference(Preferences.SAVE_ENCODING);
-  PreferenceString pSaveFormat = (PreferenceString) prefs.getPreference(Preferences.SAVE_FORMAT);
+	public void applyCurrentToApplication() {
+		Preferences prefs = (Preferences) GUITreeLoader.reg.get(GUITreeComponentRegistry.PREFERENCES);
+		PreferenceString pFileProtocol = (PreferenceString) prefs.getPreference(Preferences.FILE_PROTOCOL);
+		PreferenceLineEnding pLineEnd = (PreferenceLineEnding) prefs.getPreference(Preferences.SAVE_LINE_END);
+		PreferenceString pSaveEncoding = (PreferenceString) prefs.getPreference(Preferences.SAVE_ENCODING);
+		PreferenceString pSaveFormat = (PreferenceString) prefs.getPreference(Preferences.SAVE_FORMAT);
 
-  // Update Document Settings
-  for (int i = 0; i < Outliner.openDocumentCount(); i++) {
-   OutlinerDocument doc = Outliner.getDocument(i);
+		// Update Document Settings
+		for (int i = 0; i < Outliner.openDocumentCount(); i++) {
+		 OutlinerDocument doc = Outliner.getDocument(i);
 
-   // Only update files that do not have overriding document settings.
-   if (!doc.settings.useDocumentSettings) {
-    doc.settings.fileProtocol.def = pFileProtocol.tmp;
-    doc.settings.fileProtocol.cur = pFileProtocol.tmp;
-    doc.settings.fileProtocol.tmp = pFileProtocol.tmp;
-    doc.settings.lineEnd.def = pLineEnd.tmp;
-    doc.settings.lineEnd.cur = pLineEnd.tmp;
-    doc.settings.lineEnd.tmp = pLineEnd.tmp;
-    doc.settings.saveEncoding.def = pSaveEncoding.tmp;
-    doc.settings.saveEncoding.cur = pSaveEncoding.tmp;
-    doc.settings.saveEncoding.tmp = pSaveEncoding.tmp;
-    doc.settings.saveFormat.def = pSaveFormat.tmp;
-    doc.settings.saveFormat.cur = pSaveFormat.tmp;
-    doc.settings.saveFormat.tmp = pSaveFormat.tmp;
-    //doc.setFileModified(true);
-   }
-  }
+			// Only update files that do not have overriding document settings.
+			if (!doc.settings.useDocumentSettings) {
+				doc.settings.fileProtocol.def = pFileProtocol.tmp;
+				doc.settings.fileProtocol.cur = pFileProtocol.tmp;
+				doc.settings.fileProtocol.tmp = pFileProtocol.tmp;
+				doc.settings.lineEnd.def = pLineEnd.tmp;
+				doc.settings.lineEnd.cur = pLineEnd.tmp;
+				doc.settings.lineEnd.tmp = pLineEnd.tmp;
+				doc.settings.saveEncoding.def = pSaveEncoding.tmp;
+				doc.settings.saveEncoding.cur = pSaveEncoding.tmp;
+				doc.settings.saveEncoding.tmp = pSaveEncoding.tmp;
+				doc.settings.saveFormat.def = pSaveFormat.tmp;
+				doc.settings.saveFormat.cur = pSaveFormat.tmp;
+				doc.settings.saveFormat.tmp = pSaveFormat.tmp;
+				//doc.setFileModified(true);
+			}
+		}
 
-  // Synchronize default protocol in model.
-  Outliner.fileProtocolManager.synchronizeDefault();
-  Outliner.fileProtocolManager.synchonizeDefaultMenuItem();
+		// Synchronize default protocol in model.
+		Outliner.fileProtocolManager.synchronizeDefault();
+		Outliner.fileProtocolManager.synchonizeDefaultMenuItem();
 
 
- }
+	}
 }
