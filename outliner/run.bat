c8111720e8f60e8efa2d78b16da4a1137a7d09cc
maynardd <>
2003-02-17 07:54:43 +0000

    cleaning up

--- outliner/run.bat
+++ outliner/run.bat
@@ -9,7 +9,6 @@ set CP=%root_path%com.organic.maynard.jar
 set CP=%CP%;%root_path%outliner.jar
 set CP=%CP%;%root_path%com.yearahead.io.webfile.jar
 set CP=%CP%;%root_path%com.psm.wiki.jar
-set CP=%CP%;%root_path%sax.jar
 set CP=%CP%;%root_path%xp.jar
 set CP=%CP%;%root_path%xmlrpc.jar
 set CP=%CP%;%root_path%bsh.jar
@@ -26,9 +25,6 @@ set CP=%CP%;%module_root_path%xml\lib\jep210.jar
 :: java -cp %CP% -Duser.language=ja -Duser.region=JP -Xincgc com.organic.maynard.outliner.Outliner
 :: java -cp %CP% -Duser.language=de -Duser.region=DE -Xincgc com.organic.maynard.outliner.Outliner
 :: java -cp %CP% -Duser.language=es -Duser.region=ES -Xincgc com.organic.maynard.outliner.Outliner
-
-::java -cp %CP% -Xincgc com.organic.maynard.outliner.Outliner "%1"
-%JAVA_HOME%\bin\java -cp %CP% -Xincgc com.organic.maynard.outliner.Outliner "%1"
-::c:\jdk1.3.1\bin\java -cp %CP% -Xincgc com.organic.maynard.outliner.Outliner "%1"
+java -cp %CP% -Xincgc com.organic.maynard.outliner.Outliner "%1"
 
 @echo on
